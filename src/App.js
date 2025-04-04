93c5b9c33fdebe528236ecf84b6bf881f5e1395e
Maheshvenkata133 <96299471+Maheshvenkata133@users.noreply.github.com>
2025-04-04 18:13:47 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Telugu Evolution</h1>
+      <form>
+        <input type="text" placeholder="Username" required /><br />
+        <input type="password" placeholder="Password" required /><br />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
 
